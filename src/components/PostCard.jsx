d17a4ca33cc5e6b7ff3c65680261a5072814a622
Aagam2166 <aagam2166@gmail.com>
2025-12-02 00:51:49 +0530

    fixed image issue, updated file view, improved post card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -8,7 +8,7 @@ function PostCard({ $id, title, featuredImage }) {
             <div className='w-full bg-gray-100 rounded-xl p-4'>
                 <div className='w-full flex justify-center mb-4'>
                     <img
-  src={appwriteService.getFilePreview(featuredImage)}
+  src={appwriteService.getFileView(featuredImage)}
   alt={title}
   className='w-full h-auto rounded-xl object-cover'
 />
